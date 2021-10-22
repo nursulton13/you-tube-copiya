3bf5778d1b43df53a57c33faa157c585536768b5
nursulton13 <nursultonahmadov65@gmail.com>
2021-10-22 21:40:27 +0500

    you-tube-copiya web site first commit

---
+++ Js/JScript.js
@@ -0,0 +1,16 @@
+function myFunction() {
+   document.getElementById("second-page").style.display = "block";
+}
+function myFunction2() {
+   document.getElementById("second-page").style.display = "none";
+}
+function qidiruv() {
+   document.getElementById("left-icon").style.display = "flex";
+   document.getElementById("navbar-logo").style.display = "none";
+   document.getElementById("navbar-screen-notice").style .display ="none"
+}
+function yashir() {
+   document.getElementById("left-icon").style.display = "none";
+   document.getElementById("navbar-logo").style.display = "flex";
+   document.getElementById("navbar-screen-notice").style .display ="flex"
+}
